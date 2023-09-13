5a788be3373a7f7debd15143533f5e4877ccb065
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-09-14 03:03:46 +0600

    blogs displayed from json file

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,27 @@
 import PropTypes from 'prop-types'; // ES6
 
-const Blog = ({blog}) => {
+const Blog = ({ blog }) => {
+    const { blog_title, posted_date,hashtags, blog_cover, reading_time, author_name, author_img } = blog;
+    // console.log(blog);
     return (
-        <div>
-            <h2>{blog}</h2>
+        <div className='my-10 space-y-3 p-4'>
+            <img src={blog_cover} alt="" />
+            <div className='flex items-center gap-4'>
+
+                <img src={author_img} alt="" className='h-[50px] w-[50px] rounded-full' />
+                <div className='flex w-full justify-between items-center'>
+                    <div>
+                        <h3 className='font-semibold text-2xl'>{author_name}</h3>
+                        <p className='text-gray-500'>{posted_date}</p>
+                    </div>
+                    <div>
+                        <p className='text-gray-500'>{reading_time} Minutes</p>
+                    </div>
+                </div>
+            </div>
+            <h2 className='font-bold text-4xl'>{blog_title}</h2>
+            <p className='text-gray-500'>{hashtags}</p>
+            <a href="#" className='underline text-blue-400'>Mark as read</a>
         </div>
     );
 };
